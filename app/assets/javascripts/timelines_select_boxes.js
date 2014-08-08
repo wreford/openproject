d39dd47ba607a91db292ede4df9805f5953ca28d
jwollert <johannes.wollert@gmail.com>
2014-08-08 14:58:19 +0200

    remove all timelines related selectbox initialization from jquery code

--- app/assets/javascripts/timelines_select_boxes.js
+++ app/assets/javascripts/timelines_select_boxes.js
@@ -29,24 +29,6 @@
 //requires 'autocompleter'
 
 jQuery(document).ready(function($) {
-  [
-    $("#reporting_reported_project_status_id"),
-    $("#timeline_options_initial_outline_expansion"),
-    $("#timeline_options_zoom_factor"),
-    $("#timeline_options_compare_to_relative_unit"),
-    $("#timeline_options_grouping_one_sort"),
-    $("#timeline_options_project_sort"),
-    $("#timeline_options_grouping_two_sort"),
-    $("#timeline_options_planning_element_time_relative_one_unit"),
-    $("#timeline_options_planning_element_time_relative_two_unit")
-  ].forEach(function (item) {
-    $("input[name='" + $(item).attr("name")+"']").remove();
-
-    $(item).select2({
-      'minimumResultsForSearch': 12
-    });
-  });
-
   $(".cf_boolean_select").each(function (i, item) {
     $("input[name='" + $(item).attr("name")+"']").remove();
 
@@ -59,36 +41,6 @@ jQuery(document).ready(function($) {
     $(item).autocomplete({
       'multiple': true
     });
-  });
-
-
-  [
-    $("#timeline_options_project_responsibles"),
-    $("#timeline_options_project_status"),
-    $("#timeline_options_project_types"),
-    $("#timeline_options_planning_element_responsibles"),
-    $("#timeline_options_planning_element_assignee"),
-    $("#timeline_options_grouping_two_selection")
-  ].forEach(function (item) {
-    $(item).autocomplete({ multiple: true,
-                           ajax: {null_element: {id: -1, name: I18n.t("js.filter.noneElement")}}
-                        });
-  });
-
-  [
-    $("#timeline_options_planning_element_types"),
-    $("#timeline_options_planning_element_time_types"),
-    $("#timeline_options_planning_element_status")
-  ].forEach(function (item) {
-    $(item).autocomplete({
-      multiple: true
-    });
-  });
-
-  var item = $("#timeline_options_columns_");
-  item.autocomplete({
-    multiple: true,
-    sortable: true
   });
 
   [
@@ -107,43 +59,6 @@ jQuery(document).ready(function($) {
                           jQuery.proxy(openProject, 'fetchProjects', item.attr("data-ajaxURL")),
                           20),
       ajax: {}
-    });
-  });
-
-  [
-    $("#timeline_options_grouping_one_selection")
-  ].forEach(function (item) {
-    // Stuff borrowed from Core application.js Project Jump Box
-    $(item).autocomplete({
-      multiple: true,
-      sortable: true,
-      formatSelection: function (item) {
-        return item.name || item.project.name;
-      },
-      formatResult : OpenProject.Helpers.Search.formatter,
-      matcher      : OpenProject.Helpers.Search.matcher,
-      query        : OpenProject.Helpers.Search.projectQueryWithHierarchy(
-                          jQuery.proxy(openProject, 'fetchProjects'),
-                          20),
-      ajax: {null_element: {id: -1, name: I18n.t("js.filter.noneElement")}}
-    });
-  });
-
-  [
-    $("#timeline_options_parents")
-  ].forEach(function (item) {
-    // Stuff borrowed from Core application.js Project Jump Box
-    $(item).autocomplete({
-      multiple: true,
-      formatSelection: function (item) {
-        return item.name || item.project.name;
-      },
-      formatResult : OpenProject.Helpers.Search.formatter,
-      matcher      : OpenProject.Helpers.Search.matcher,
-      query        : OpenProject.Helpers.Search.projectQueryWithHierarchy(
-                          jQuery.proxy(openProject, 'fetchProjects'),
-                          20),
-      ajax: {null_element: {id: -1, name: I18n.t("js.filter.noneElement")}}
     });
   });
 
